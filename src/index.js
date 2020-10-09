3e322a37c4664d965355d12c2a91b7b0b7aed6ca
Pavlo Sliusarchuk <sksuzh@gmail.com>
2020-10-09 23:02:47 +0300

    feat: add decode function implementation

--- src/index.js
+++ src/index.js
@@ -38,7 +38,50 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  function getLetterListFromString(encrStr = '') {
+    let letterList = [];
+    
+    for(let i = 0; i < encrStr.length / 10; i++){
+      const element = encrStr.slice(i*10, i*10+10);
+      letterList.push(element);
+    }
+    return letterList;
+  }
+
+  function deleteSpareLeftZero (argArr = []) {
+    return argArr.map((item) => parseFloat(item).toString());
+  }
+
+  function convertListToMorseList (binaryList = []) {
+    let morseList = binaryList.map( (binaryItem) => {
+      let morseItem = '';
+      if(isNaN(binaryItem)) return ' ';
+      for (let i = 0; i < binaryItem.length /2; i++) {
+        const binaryDotDash = binaryItem.substr(i*2, 2);
+        morseItem += binaryDotDash === '10' ? '.' : '-';
+      }
+      return morseItem;
+    });
+    return morseList; 
+  }
+
+  function convertMorseListToEngList (morseList = []) {
+    let engList = morseList.map(morseItem => {
+      if(morseItem === ' ') return morseItem;
+      const engLetter =  MORSE_TABLE[morseItem]; 
+      return engLetter;
+    });
+    return engList;
+  }
+
+  if(expr.length % 10 !== 0) throw new Error("The string's length is not  multiple of 10." );
+
+  let result = getLetterListFromString(expr);
+  result = deleteSpareLeftZero(result);
+  result = convertListToMorseList(result);
+  result = convertMorseListToEngList(result);
+  result = result.join('');
+  return result;
 }
 
 module.exports = {
